32eae81ec840188b85b94cbd7c0940adbca02630
Jason Crist <jcrist@pbking.com>
2020-11-09 13:17:11 -0500

    Resyncing changes from SVN

--- morden/header.php
+++ morden/header.php
@@ -10,11 +10,6 @@
  * @subpackage Varia
  * @since 1.0.0
  */
-$has_primary_nav = has_nav_menu( 'menu-1' );
-$header_classes  = 'site-header-wrap responsive-max-width';
-$header_classes .= has_custom_logo() ? ' has-logo' : '';
-$header_classes .= 1 === get_theme_mod( 'header_text', true ) ? ' has-title-and-tagline' : '';
-$header_classes .= $has_primary_nav ? ' has-menu' : '';
 ?><!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
@@ -31,13 +26,13 @@ $header_classes .= $has_primary_nav ? ' has-menu' : '';
 		wp_body_open();
 	}
 ?>
-
+	
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'varia' ); ?></a>
 
 	<?php if ( class_exists( 'A8C\FSE\WP_Template' ) ) : // If the FSE plugin is active, use the Header template for content. ?>
 
-		<header id="masthead" class="fse-template-part fse-header entry-content">
+		<header class="fse-template-part fse-header entry-content">
 			<?php
 				$template = new A8C\FSE\WP_Template();
 				$template->output_template_content( A8C\FSE\WP_Template::HEADER );
@@ -45,13 +40,15 @@ $header_classes .= $has_primary_nav ? ' has-menu' : '';
 		</header>
 
 	<?php else : // Otherwise we'll fallback to the default Varia header below. ?>
-		<header id="masthead" class="site-header" role="banner">
-			<div class="<?php echo $header_classes; ?>">
+
+		<header id="masthead" class="site-header">
+
+			<div class="site-header-wrap">
+
 				<?php get_template_part( 'template-parts/header/site', 'branding' ); ?>
 
-				<?php if ( $has_primary_nav ) : ?>
+				<?php if ( has_nav_menu( 'menu-1' ) ) : ?>
 					<nav id="site-navigation" class="main-navigation" aria-label="<?php esc_attr_e( 'Main Navigation', 'varia' ); ?>">
-
 						<input type="checkbox" role="button" aria-haspopup="true" id="toggle" class="hide-visually">
 						<label for="toggle" id="toggle-menu" class="button">
 							<?php _e( 'Menu', 'varia' ); ?>
@@ -60,7 +57,6 @@ $header_classes .= $has_primary_nav ? ' has-menu' : '';
 							<span class="hide-visually expanded-text"><?php _e( 'expanded', 'varia' ); ?></span>
 							<span class="hide-visually collapsed-text"><?php _e( 'collapsed', 'varia' ); ?></span>
 						</label>
-
 						<?php
 						wp_nav_menu(
 							array(
@@ -74,7 +70,7 @@ $header_classes .= $has_primary_nav ? ' has-menu' : '';
 				<?php endif; ?>
 
 				<?php if ( has_nav_menu( 'social' ) ) : ?>
-					<nav class="social-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Social Links Menu', 'varia' ); ?>">
+					<nav class="social-navigation" aria-label="<?php esc_attr_e( 'Social Links Menu', 'varia' ); ?>">
 						<?php
 						wp_nav_menu(
 							array(
@@ -89,7 +85,7 @@ $header_classes .= $has_primary_nav ? ' has-menu' : '';
 					</nav><!-- .social-navigation -->
 				<?php endif; ?>
 
-			</div><!-- .site-header-wrap -->
+			</div>
 
 		</header><!-- #masthead -->
 
--- seedlet/template-parts/footer/footer-widgets.php
+++ seedlet/template-parts/footer/footer-widgets.php
@@ -9,9 +9,15 @@
 if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
 
 	<div class="widget-area">
-		<div class="widget-column footer-widget-1">
-		<?php dynamic_sidebar( 'sidebar-1' ); ?>
-		</div>
+		<?php
+		if ( is_active_sidebar( 'sidebar-1' ) ) {
+			?>
+					<div class="widget-column footer-widget-1">
+					<?php dynamic_sidebar( 'sidebar-1' ); ?>
+					</div>
+				<?php
+		}
+		?>
 	</div><!-- .widget-area -->
 
 <?php endif; ?>
